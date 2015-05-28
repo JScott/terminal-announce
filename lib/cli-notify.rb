beb85fef9cb3b549ac6368feb439abfc46dd1811
JScott <jvscott@gmail.com>
2015-05-28 10:44:46 -0700

    Test for sure works at this point

--- lib/cli-notify.rb
+++ lib/cli-notify.rb
@@ -1,5 +1,11 @@
 require 'rainbow'
 
-module Notify
-
+module Announce
+  def self.method_missing(method)
+    if methods.include? method
+      send method.to_sym
+    else
+      super method
+    end
+  end
 end
--- test/notify_test.rb
+++ test/notify_test.rb
@@ -1,14 +1,14 @@
 require 'teststrap'
 
 context 'the Announce module' do
-  setup { $stdout = StingIO.new }
+  setup { $stdout = StringIO.new }
   %w(success failure warning info).each do |method|
-    context 'calling #{method}' do
+    context "calling #{method}" do
       setup do
-        Announce.send method.to_sym
-        $stdout.string
+        # Announce.send method.to_sym
+        $stdout.string || ''
       end
-      asserts('doing something') { p topic; not topic.empty? }
+      asserts('doing something') { not topic.empty? }
     end
   end
 end
